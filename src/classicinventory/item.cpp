499e40ce760d2abcdef946b7510e8a2c6dba5a11
Jacques Niemand <jmniemand@gmail.com>
2018-12-25 21:59:07 +0200

    Fix bug allowing unlimited medipacks via shortcut keys

--- src/classicinventory/item.cpp
+++ src/classicinventory/item.cpp
@@ -60,20 +60,24 @@ bool ItemQuantity::set(int32_t qty) const
 }
 
 bool ItemQuantity::increase(int32_t qty) const {
-	const auto quantity = get();
+	const auto qty_old = get();
 
-	if (quantity != ITEM_QTY_UNLIMITED) {
-		return set(min(quantity_max, quantity + qty));
+	if (qty_old != ITEM_QTY_UNLIMITED) {
+		set(min(quantity_max, qty_old + qty));
+
+		return get() != qty_old;
 	}
 
 	return false;
 }
 
 bool ItemQuantity::decrease(int32_t qty) const {
-	const auto quantity = get();
+	const auto qty_old = get();
 
-	if (quantity != ITEM_QTY_UNLIMITED) {
-		return set(max(quantity_min, quantity - qty));
+	if (qty_old != ITEM_QTY_UNLIMITED) {
+		set(max(quantity_min, qty_old - qty));
+
+		return get() != qty_old;
 	}
 
 	return false;
